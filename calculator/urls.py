454dc519427ff20840a06835381b05a787e08c44
KiberPerdun <Suisuikin@yandex.ru>
2025-12-29 11:45:30 +0300

    move

--- calculator/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('calculate/', views.calculate_chrono, name='calculate_chrono'),
-    path('health/', views.health_check, name='health'),
-]
